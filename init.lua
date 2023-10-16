9630ad87b75ad38e16ecc9ed28cf4456508a45f4
soulis-1256 <tasosandreou01@gmail.com>
2023-10-16 20:47:12 +0300

    lualine theme

--- init.lua
+++ init.lua
@@ -338,8 +338,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -354,12 +354,16 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = { 'mode' },
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 }
+    },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    }
   },
   inactive_sections = {
     lualine_a = {},
