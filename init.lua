f33aef692b44db5ef85731aa9f737e68fe61fbbe
soulis-1256 <tasosandreou01@gmail.com>
2023-10-17 15:42:38 +0300

    added scrollbar, copilot

--- init.lua
+++ init.lua
@@ -36,6 +36,23 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    'petertriho/nvim-scrollbar',
+  },
+  {
     "windwp/nvim-autopairs",
     opts = {
       fast_wrap = {},
@@ -336,7 +353,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+--vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -370,6 +387,196 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+require('copilot').setup({
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = false,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 16.x
+  server_opts_overrides = {},
+})
+
+require("scrollbar").setup({
+  show = true,
+  show_in_active_only = false,
+  set_highlights = true,
+  folds = 1000,                -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
+  max_lines = false,           -- disables if no. of lines in buffer exceeds this
+  hide_if_all_visible = false, -- Hides everything if all lines are visible
+  throttle_ms = 100,
+  handle = {
+    text = " ",
+    blend = 30,                 -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+    color = nil,
+    color_nr = nil,             -- cterm
+    highlight = "CursorColumn",
+    hide_if_all_visible = true, -- Hides handle if all lines are visible
+  },
+  marks = {
+    Cursor = {
+      text = "•",
+      priority = 0,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "Normal",
+    },
+    Search = {
+      text = { "-", "=" },
+      priority = 1,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "Search",
+    },
+    Error = {
+      text = { "-", "=" },
+      priority = 2,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "DiagnosticVirtualTextError",
+    },
+    Warn = {
+      text = { "-", "=" },
+      priority = 3,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "DiagnosticVirtualTextWarn",
+    },
+    Info = {
+      text = { "-", "=" },
+      priority = 4,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "DiagnosticVirtualTextInfo",
+    },
+    Hint = {
+      text = { "-", "=" },
+      priority = 5,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "DiagnosticVirtualTextHint",
+    },
+    Misc = {
+      text = { "-", "=" },
+      priority = 6,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "Normal",
+    },
+    GitAdd = {
+      text = "┆",
+      priority = 7,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "GitSignsAdd",
+    },
+    GitChange = {
+      text = "┆",
+      priority = 7,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "GitSignsChange",
+    },
+    GitDelete = {
+      text = "▁",
+      priority = 7,
+      gui = nil,
+      color = nil,
+      cterm = nil,
+      color_nr = nil, -- cterm
+      highlight = "GitSignsDelete",
+    },
+  },
+  excluded_buftypes = {
+    "terminal",
+  },
+  excluded_filetypes = {
+    "cmp_docs",
+    "cmp_menu",
+    "noice",
+    "prompt",
+    "TelescopePrompt",
+  },
+  autocmd = {
+    render = {
+      "BufWinEnter",
+      "TabEnter",
+      "TermEnter",
+      "WinEnter",
+      "CmdwinLeave",
+      "TextChanged",
+      "VimResized",
+      "WinScrolled",
+    },
+    clear = {
+      "BufWinLeave",
+      "TabLeave",
+      "TermLeave",
+      "WinLeave",
+    },
+  },
+  handlers = {
+    cursor = true,
+    diagnostic = true,
+    gitsigns = false, -- Requires gitsigns
+    handle = true,
+    search = false,   -- Requires hlslens
+    ale = false,      -- Requires ALE
+  },
+})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -632,7 +839,6 @@ require('which-key').register {
   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 }
 
@@ -745,5 +951,6 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
+    { name = 'copilot' }
   },
 }
