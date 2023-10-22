47dc5267b8c9c44d165aa9aef571479d0513ef16
soulis-1256 <tasosandreou01@gmail.com>
2023-10-22 17:20:44 +0300

    improved hover tips

--- init.lua
+++ init.lua
@@ -420,151 +420,9 @@ require('copilot').setup({
   server_opts_overrides = {},
 })
 
-require("scrollbar").setup({
-  show = true,
-  show_in_active_only = false,
-  set_highlights = true,
-  folds = 1000,                -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
-  max_lines = false,           -- disables if no. of lines in buffer exceeds this
-  hide_if_all_visible = false, -- Hides everything if all lines are visible
-  throttle_ms = 100,
-  handle = {
-    text = " ",
-    blend = 30,                 -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
-    color = nil,
-    color_nr = nil,             -- cterm
-    highlight = "CursorColumn",
-    hide_if_all_visible = true, -- Hides handle if all lines are visible
-  },
-  marks = {
-    Cursor = {
-      text = "•",
-      priority = 0,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "Normal",
-    },
-    Search = {
-      text = { "-", "=" },
-      priority = 1,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "Search",
-    },
-    Error = {
-      text = { "-", "=" },
-      priority = 2,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "DiagnosticVirtualTextError",
-    },
-    Warn = {
-      text = { "-", "=" },
-      priority = 3,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "DiagnosticVirtualTextWarn",
-    },
-    Info = {
-      text = { "-", "=" },
-      priority = 4,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "DiagnosticVirtualTextInfo",
-    },
-    Hint = {
-      text = { "-", "=" },
-      priority = 5,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "DiagnosticVirtualTextHint",
-    },
-    Misc = {
-      text = { "-", "=" },
-      priority = 6,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "Normal",
-    },
-    GitAdd = {
-      text = "┆",
-      priority = 7,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "GitSignsAdd",
-    },
-    GitChange = {
-      text = "┆",
-      priority = 7,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "GitSignsChange",
-    },
-    GitDelete = {
-      text = "▁",
-      priority = 7,
-      gui = nil,
-      color = nil,
-      cterm = nil,
-      color_nr = nil, -- cterm
-      highlight = "GitSignsDelete",
-    },
-  },
-  excluded_buftypes = {
-    "terminal",
-  },
-  excluded_filetypes = {
-    "cmp_docs",
-    "cmp_menu",
-    "noice",
-    "prompt",
-    "TelescopePrompt",
-  },
-  autocmd = {
-    render = {
-      "BufWinEnter",
-      "TabEnter",
-      "TermEnter",
-      "WinEnter",
-      "CmdwinLeave",
-      "TextChanged",
-      "VimResized",
-      "WinScrolled",
-    },
-    clear = {
-      "BufWinLeave",
-      "TabLeave",
-      "TermLeave",
-      "WinLeave",
-    },
-  },
-  handlers = {
-    cursor = true,
-    diagnostic = true,
-    gitsigns = false, -- Requires gitsigns
-    handle = true,
-    search = false,   -- Requires hlslens
-    ale = false,      -- Requires ALE
-  },
-})
+-- [[ Configure scrollbar ]]
+-- Check handlers/config.lua
+require("scrollbar").setup()
 
 vim.diagnostic.config({
   virtual_text = false, -- Turn off inline diagnostics
@@ -965,13 +823,12 @@ cmp.setup {
   },
 }
 
-
 -- Experimental code start 1256
 local error_win = nil
 
 -- Function to create a floating window at specific screen coordinates
 function create_float_window(row, col, width, height)
-  local float_opts = {
+  --[[local float_opts = {
     relative = 'editor',
     row = row,
     col = col,
@@ -979,11 +836,17 @@ function create_float_window(row, col, width, height)
     height = height,
     style = 'minimal',
     border = 'single',
-    focusable = false,
-  }
+    focusable = true,
+  }]]
 
   local buf = vim.api.nvim_create_buf(false, true) -- Create an empty buffer
-  local win = vim.api.nvim_open_win(buf, false, float_opts)
+  local _, win = vim.diagnostic.open_float(0, {
+    border = "single",
+    focusable = true,
+    focus = false,
+  })
+  vim.api.nvim_win_set_width(win, vim.api.nvim_win_get_width(win) + 1)
+  --local win = vim.api.nvim_open_win(buf, false, float_opts)
 
   return win, buf
 end
@@ -1000,12 +863,12 @@ function show_diagnostics()
   local diagnostics = vim.diagnostic.get(0, { bufnr = bufnr })
 
   local has_diagnostics = false
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
   local ok, mouse_pos = pcall(vim.fn.getmousepos)
   if ok then
     for _, diagnostic in ipairs(diagnostics) do
-      if diagnostic.lnum == mouse_pos.line - 1 then
+      if diagnostic.lnum == (mouse_pos.line - 1) and diagnostic.lnum == (cursor_pos[1] - 1) then
         has_diagnostics = true
-
 
         -- Create or update the floating window
         if error_win and vim.api.nvim_win_is_valid(error_win) then
@@ -1015,8 +878,7 @@ function show_diagnostics()
         error_win, error_buf = create_float_window(mouse_pos.screenrow, mouse_pos.screencol, 40, 5)
 
         -- Set the content of the floating window with the diagnostic message
-        vim.api.nvim_buf_set_lines(error_buf, 0, -1, false, vim.split(diagnostic.message, "\n"))
-
+        --vim.api.nvim_buf_set_lines(error_buf, 0, -1, false, vim.split(diagnostic.message, "\n"))
         break
       end
     end
